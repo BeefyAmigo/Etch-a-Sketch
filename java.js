fa2b1c274bfad828d881f1238da52b0c781498e3
BeefyAmigo <alex.houser67@gmail.com>
2024-11-05 18:28:25 -0600

    Move event listener into loop

--- java.js
+++ java.js
@@ -1,12 +1,10 @@
 const container = document.querySelector('#container');
-const grid = document.querySelector('.grid');
 
 for (i=1; i<273; i++) {
     let div = document.createElement('div');
-    div.classList.add('grid');
     container.appendChild(div);
+    div.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'black';
+    });
 }
 
-container.addEventListener('mouseover', (event) => {
-    event.target.style.backgroundColor = 'black';
-});
