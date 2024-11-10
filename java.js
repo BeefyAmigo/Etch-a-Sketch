157d314368c2bb1962f1c833d6a235b99fb48152
BeefyAmigo <alex.houser67@gmail.com>
2024-11-10 14:33:48 -0600

    Make button delete the sketch
    I made the button delete the sketch as well it
    gives the user an alert if the num entered in the
    prompt is less than 1 or more than 100

--- java.js
+++ java.js
@@ -1,5 +1,12 @@
+const button = document.querySelector('button');
+const sketch = document.querySelector('.sketch-container');
+
 function createGrid(userInput) {
-    let screen = document.querySelector('.sketch-screen');
+    let screen = document.createElement('div');
+    screen.classList.add('sketch-screen');
+    sketch.appendChild(screen);
+
+    
     for (i = 0; i < userInput; i++) {
     
         let col = document.createElement('div');
@@ -20,13 +27,19 @@ function createGrid(userInput) {
         
     }
 }
-//function setGridSize() {
-    //let button = document.querySelector('button');
-    //button.addEventListener('click', () => {
-         //let userInput = prompt('Enter Custom Grid Size');
-         //createGrid(userInput);
-    //})
+
+button.addEventListener('click', () => {
+    let userInput = prompt('Enter Custom Grid Size min:1 max:100');
+    if(userInput < 101 && userInput > 0){
+        let screen = document.querySelector('.sketch-screen')
+        screen.parentNode.removeChild(screen);
+        createGrid(userInput);
+    }
+    else {
+        alert('Please use a number from 1-100');
+    }
     
-//}
+})
 createGrid(16);
+
 
